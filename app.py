6333806e324a0045af34c72df6593542e4a73676
Andrea Cea Villadeo <andreacea96@gmail.com>
2025-11-25 22:36:53 -0600

    up

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def delete_definicion(termino):
 from metodos_numericos import metodos_numericos
 from metodos_numericos_dos import metodos_numericos_dos
 from ejemplos.ejemplos import app as ejemplos_app  # CORREGIDO: import correcto de la función app
-from ejemplos2.ejemplos2 import app as ejemplos_app  # CORREGIDO: import correcto de la función app
+from ejemplos2.ejemplos2 import app as ejemplos2_app  # CORREGIDO: import correcto de la función app
 
 # ========================
 # Configuración
@@ -164,4 +164,4 @@ elif menu == "Métodos Numéricos II":
 elif menu == "Ejemplos Métodos Numéricos I":
     ejemplos_app()
 elif menu == "Ejemplos Métodos Numéricos II":
-    ejemplos_app()
+    ejemplos2_app()
